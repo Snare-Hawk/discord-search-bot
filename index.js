9a2b6565f013036b5ab4d7f801cbf21513ba1879
Snare-Hawk <85657152+Snare-Hawk@users.noreply.github.com>
2022-11-22 21:17:59 -0500

    i probably should just make a new repo

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
-require("dotenv").config();
-const { Client, Intents } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const {
+    Client,
+    Collection,
+    Events,
+    GatewayIntentBits,
+    ActivityType
+} = require("discord.js");
+const { token } = require("./config.json");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+}
+
+client.once(Events.ClientReady, () => {
+    console.log("Ready!");
+    client.user.setPresence({
+        activities: [{ name: "Spy X Family", type: ActivityType.Watching }],
+        status: "dnd"
+    });
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: "There was an error while executing this command!",
+            ephemeral: true
+        });
+    }
+});
+
+/*
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions
+    ]
+});
 
 const character = require("./character");
 const media = require("./media");
 const staff = require("./staff");
 const user = require("./user");
 const studio = require("./studio");
-
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS
-    ],
-    disableMentions: "everyone"
-});
 
 const deleteViaReaction = require("./deleteViaReaction");
 
@@ -27,9 +75,19 @@ client.on("ready", () => {
         `Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`
     );
     console.log(
-        `\nAdd the bot to your server here:\nhttps://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=1024`
+        `\nAdd the bot to your server here:\nhttps://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8`
     );
-    client.user.setActivity("Spy X Family", { type: "WATCHING" });
+
+    client.user.setPresence({
+        activities: [{ name: "Spy X Family", type: ActivityType.Watching }],
+        status: "dnd"
+    });
+    // client.process.send("997182942759628833", "yop");
+    // client.users.send("301082610438897664", "I'm online now!");
+
+    // const channel = client.channels.cache.get("975876824972283944");
+
+    // channel.send("Watch Spy X Family!");
 });
 
 client.on("messageCreate", async message => {
@@ -50,12 +108,13 @@ client.on("messageCreate", async message => {
     let response = null;
 
     switch (command) {
+        case "h":
         case "help":
             response = help;
             break;
 
-        case "invite":
         case "i":
+        case "invite":
             response = invite;
             break;
 
@@ -89,8 +148,13 @@ client.on("messageCreate", async message => {
         case "user":
             response = await user.search(args);
             break;
+
         case "anya":
             response = anya;
+            break;
+
+        case "react":
+            response = react;
             break;
     }
 
@@ -146,6 +210,15 @@ const invite = {
     description: `You can invite me [here](https://discordapp.com/oauth2/authorize?client_id=883945081856479252&scope=bot&permissions=1024)!`
 };
 
+const react = {
+    //     client.once(Events, ClientReady, () => {
+    //     console.log("Ready!");
+    // });
+    // client.on(Events, InteractionCreate, interaction => {
+    //     message.react("😄");
+    // });
+};
+
 const anya = {
     title: "waku waku",
     image: {
@@ -153,5 +226,5 @@ const anya = {
             "https://cdn.discordapp.com/app-assets/883945081856479252/1027398078522740737.png"
     }
 };
-
-client.login(process.env.TOKEN);
+*/
+client.login(token);
